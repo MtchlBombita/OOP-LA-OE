f2b0fbbd5410d0e84c7f5d4e6ce2da1e22d68495
MtchlBombita <castilloalexandria178@gmail.com>
2024-12-22 01:05:59 +0800

    Create LA3#.py

---
+++ Learning Assessment/01/LA3#.py
@@ -0,0 +1,19 @@
+
+class classification():
+    def __init__(self, name, bsc_atck, role):
+        self.name = name 
+        self.bsc_atck = bsc_atck
+        self.role = role
+
+    def describe(self):
+        return f"{self.name} is a {self.role} type hero"
+    
+mm1 = classification("gusion", 120, "assassin")
+mm2 = classification("fanny", 240, "assassin")
+
+print(mm1.name)
+print(mm1.bsc_atck)
+print(mm1.describe())
+print(mm2.name)
+print(mm2.bsc_atck)
+print(mm1.describe())
