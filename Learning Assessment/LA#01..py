d7c0a719e6040a2c2c6a96e0574a760ff7145611
MtchlBombita <castilloalexandria178@gmail.com>
2024-12-22 14:04:17 +0800

    Create LA#01..py

---
+++ Learning Assessment/LA#01..py
@@ -0,0 +1,15 @@
+class videoGame():
+    hero_name = "gusion" 
+    skill = "dagger"
+    ultimate = "teleport" 
+    role = "assassin"
+    
+name = videoGame()
+skills = videoGame()
+ss = videoGame()
+roles = videoGame()
+
+print(name.hero_name)
+print(skills.skill)
+print(ss.ultimate)
+print(roles.role)
