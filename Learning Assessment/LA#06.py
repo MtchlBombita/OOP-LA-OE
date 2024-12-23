7e443102694d9abc6fb5722154c49022125178d1
MtchlBombita <castilloalexandria178@gmail.com>
2024-12-23 11:11:47 +0800

    Create LA#06.py

---
+++ Learning Assessment/LA#06.py
@@ -0,0 +1,11 @@
+print("LA:6")
+class Laptop:
+    def __init__(self, brand, model):
+        pass
+    def laptop_info(self):
+        pass
+laptop1 = Laptop("Huawei", "Lenovo")
+laptop2 = Laptop("AlienWare", "M18")
+
+print(laptop1.laptop_info())
+print(laptop2.laptop_info())
