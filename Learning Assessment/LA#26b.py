bf3310511b3af794e9e4144fa2ef54fcbced6838
MtchlBombita <castilloalexandria178@gmail.com>
2024-12-23 11:26:43 +0800

    Create LA#26b.py

---
+++ Learning Assessment/LA#26b.py
@@ -0,0 +1,11 @@
+import mitchelbsit2b
+
+Leonardo = mitchelbsit2b.Leonardo ("Leonardo", "NiwNiw")
+MichaelAngelo = mitchelbsit2b.MichaelAngelo ("MichaelAngelo", "NitNit")
+Donatello = mitchelbsit2b.Donatello ("Donatello", "MitchiBoy")
+Raphael = mitchelbsit2b.Raphael ("Raphael", "Labo")
+
+print(Leonardo.alias)
+print(MichaelAngelo.alias)
+print(Donatello.alias)
+print(Raphael.alias)
